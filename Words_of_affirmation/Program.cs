8416426c1b074f8a0fb41704f6e6c316f357e48b
alexg <alexg@DESKTOP-TDPUB3O>
2022-12-03 10:59:51 +0100

    Agregar archivos de proyecto.

---
+++ Words_of_affirmation/Program.cs
@@ -0,0 +1,21 @@
+﻿using Words_of_affirmation;
+
+int timeInterval = 0;
+
+Console.WriteLine("Introduce the time interval");
+
+timeInterval = int.Parse(Console.ReadLine());
+
+TimeElapsed tm = new TimeElapsed(timeInterval);
+
+while(true)
+{
+    tm.ElapsedTime();
+}
+
+
+
+
+
+
+
