09fd8e6e8f3573ea1c58f555ac299c56d1cd3ea3
alexg <alexg@DESKTOP-TDPUB3O>
2022-12-03 11:34:28 +0100

    Trial

--- Words_of_affirmation/Program.cs
+++ Words_of_affirmation/Program.cs
@@ -1,19 +1,34 @@
-﻿using Words_of_affirmation;
+﻿using System.Runtime.CompilerServices;
+using System.Timers;
 
 int timeInterval = 0;
 
 Console.WriteLine("Introduce the time interval");
 
 timeInterval = int.Parse(Console.ReadLine());
+int myTime = timeInterval * 1000;
 
-TimeElapsed tm = new TimeElapsed(timeInterval);
+System.Timers.Timer myTimer = new System.Timers.Timer();
+myTimer.Interval = myTime;
+myTimer.Elapsed += new ElapsedEventHandler(GetPhrase);
+myTimer.Enabled = true;
 
-while(true)
+Console.WriteLine("Press \'q\' to quit the sample.");
+while (Console.Read() != 'q');
+
+static void GetPhrase(object source, ElapsedEventArgs e)
 {
-    tm.ElapsedTime();
+    Random randomNumber = new Random();
+    int ran = randomNumber.Next(0, 4);
+    List<String> phrases = new List<string>()
+            {
+                "Go off, do something!",
+                "You can do it...I think",
+                "You can have a nap after this!",
+                "A llorar a la llorería",
+            };
+    Console.WriteLine((phrases[ran]));
 }
-
-
 
 
 
